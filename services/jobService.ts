f068fe38caadca3c970ee74e551a773fb489d81d
SahanRansika <sahanransika07@gmail.com>
2026-02-01 16:22:17 +0530

    Add to Job Service file

---
+++ services/jobService.ts
@@ -0,0 +1,13 @@
+import { collection, addDoc, getDocs } from "firebase/firestore";
+import { db } from "../firebase/firebase";
+
+const jobsRef = collection(db, "jobs");
+
+export const addJob = async (job: any) => {
+  await addDoc(jobsRef, job);
+};
+
+export const getJobs = async () => {
+  const snap = await getDocs(jobsRef);
+  return snap.docs.map(d => ({ id: d.id, ...d.data() }));
+};
